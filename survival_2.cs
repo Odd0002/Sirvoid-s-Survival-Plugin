4982c933580a81df91da771eaa8b18f3f2e6676e
Odd0002 <Odd0002minecraft@gmail.com>
2018-05-19 22:31:44 -0500

    Add inventory methods

--- survival_2.cs
+++ survival_2.cs
@@ -15,6 +15,7 @@ namespace MCGalaxy
 
         private Dictionary<Player, SurvivalPlayer> playerMap = new Dictionary<Player, SurvivalPlayer>();
 
+
         public override void Help(Player p)
         {
             Helpers.SendTextBlockToPlayer(p, 
@@ -40,6 +41,8 @@ namespace MCGalaxy
 
     }
 
+//==============================================================================================
+
     public struct CraftingRecipe
     {
         public string name;
@@ -48,18 +51,35 @@ namespace MCGalaxy
         public int itemID;
         public Dictionary<int, int> requiredIngredients;
     }
-    
+
+//==============================================================================================
+
+    public class SurvivalMob
+    {
+
+    }
+
+//==============================================================================================
+
 
     public class SurvivalPlayer
     {
+        string playerInvLocation = "./text/survivalPlugin/inv/";
         Player thisPlayer;
         int health;
         int breath;
         int pvpCooldown;
+        Inventory playerInventory = new Inventory();
 
         public SurvivalPlayer(Player player)
         {
             this.thisPlayer = player;
+            LoadInventory();
+        }
+
+        private void LoadInventory()
+        {
+            playerInventory.LoadItems(playerInvLocation + thisPlayer.name + ".txt");
         }
 
         public void SendMessage(string message)
@@ -78,10 +98,79 @@ namespace MCGalaxy
         }
     }
 
+//==============================================================================================
+
     public class Inventory
     {
+        Dictionary<int, int> items = new Dictionary<int, int>();
+        
+        public bool LoadItems(string filename)
+        {
+            if (!System.IO.File.Exists(filename))
+            {
+                return false;
+            }
+
+            //TODO load items
+            return false;
+        }
+
+        public bool CraftItem(CraftingRecipe recipe)
+        {
+            //Check if each of the required ingredients is present in the inventory
+            foreach (int requiredID in recipe.requiredIngredients.Keys)
+            {
+                int count;
+                items.TryGetValue(requiredID, out count);
+                if (count < recipe.requiredIngredients[requiredID])
+                {
+                    return false;
+                }
+            }
+
+            //All the required ingredients are present, remove them all before adding the crafted item
+            foreach (int requiredID in recipe.requiredIngredients.Keys)
+            {
+                RemoveItem(requiredID, recipe.requiredIngredients[requiredID]);
+            }
+
+            //Add the crafted item
+            AddItem(recipe.itemID, recipe.craftCount);
+            return true;
+        }
+
+        public void AddItem(int ID)
+        {
+            ModifyItem(ID, 1);
+        }
+
+        public void AddItem(int ID, int count)
+        {
+            ModifyItem(ID, count);
+        }
+
+        public void RemoveItem(int ID)
+        {
+            ModifyItem(ID, -1);
+        }
+
+        public void RemoveItem(int ID, int count)
+        {
+            ModifyItem(ID, -1 * count);
+        }
+
+
+        private void ModifyItem(int ID, int amountToChange)
+        {
+            int currCount;
+            items.TryGetValue(ID, out currCount);
+            items[ID] = Math.Max(0, (currCount + amountToChange));
+        }
 
     }
+
+//==============================================================================================
+
 
     public static class Helpers
     {
@@ -94,26 +183,31 @@ namespace MCGalaxy
             }
         }
 
-        public static Dictionary<string, CraftingRecipe> loadCraftsFromFile(string filename)
+        public static Dictionary<string, CraftingRecipe> GetCraftsFromFile(string filename)
         {
             Dictionary<string, CraftingRecipe> crafts = new Dictionary<string, CraftingRecipe>();
+
+            //Read in the file line by line for each recipe
             string currLine;
             System.IO.StreamReader file = new System.IO.StreamReader(filename);
+
             while ((currLine = file.ReadLine()) != null)
             {
                 //Craft info stored as: name,category,ID,quantity,ingredient1ID,ingredient1Count,ingredient2ID,ingredient2Count,... 
                 string[] craftInfo = currLine.Split(',');
-                crafts[craftInfo[0]] = createRecipie(craftInfo);
+                crafts[craftInfo[0]] = CreateRecipie(craftInfo);
             }
             return crafts;
         }
 
 
-        public static CraftingRecipe createRecipie(string[] craftInfo)
+        public static CraftingRecipe CreateRecipie(string[] craftInfo)
         {
+            //Offset is where the required ingredients list starts
             int offset = 4;
             CraftingRecipe recipe;
 
+            //Set recipe information
             recipe.name = craftInfo[0];
             recipe.category = craftInfo[1];
             recipe.itemID = Int32.Parse(craftInfo[2]);
@@ -122,6 +216,7 @@ namespace MCGalaxy
 
             int craftCount = craftInfo.Length;
 
+            //Create a dictionary of <required_item_id, required_count> pairs
             Dictionary<int, int> requiredIngredients = new Dictionary<int, int> ();
             for (int i=offset; i < craftCount; i+=2)
             {
